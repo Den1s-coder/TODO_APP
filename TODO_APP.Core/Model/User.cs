120dc7254351e2104009b1173447786198fdc88e
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-01 17:17:56 +0300

    Move Models from Web layer to Core

---
+++ TODO_APP.Core/Model/User.cs
@@ -0,0 +1,17 @@
+﻿namespace TODO_APP.Core.Model
+{ 
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public User(string name, string email, string password)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            PasswordHash = password;
+        }
+    }
+}
