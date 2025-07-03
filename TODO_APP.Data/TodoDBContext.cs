b3231be40deffbf81743a1987f3860bb1b6678e5
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-03 16:45:48 +0300

    Add references

---
+++ TODO_APP.Data/TodoDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TODO_APP.Data
+{
+    public class TodoDBContext : DbContext
+    {
+        public TodoDBContext(DbContextOptions<TodoDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
