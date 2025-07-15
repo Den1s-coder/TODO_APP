ed1bae41545e2cefe68552a818ec554f2a8bfb37
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-15 15:12:54 +0300

    Add INoteRepository interface and NoteRepository implementation with methods for working with notes in the DB.

--- TODO_APP.Data/Repos/NoteRepository.cs
+++ TODO_APP.Data/Repos/NoteRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace TODO_APP.Data.Repos
+﻿using TODO_APP.Core.Model;
+using TODO_APP.Data.Repos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace TODO_APP.Data.Repos
 {
-    public class NoteRepository
+    public class NoteRepository : INoteRepository
     {
+        private readonly TodoDBContext _context;
+
+        public NoteRepository(TodoDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Note> GetByIdAsync(int id)
+        {
+            return await _context.Notes.FindAsync(id) ?? throw new Exception("Note not found");
+        }
+
+        public async Task<IEnumerable<Note>> GetAllAsync()
+        {
+            return await _context.Notes.ToListAsync();
+        }
+
+        public async Task AddAsync(Note note)
+        {
+            await _context.Notes.AddAsync(note);
+        }
+
+        public void Update(Note note)
+        {
+            _context.Notes.Update(note);
+        }
+
+        public void Delete(Note note)
+        {
+            _context.Notes.Remove(note);
+        }
     }
 }
