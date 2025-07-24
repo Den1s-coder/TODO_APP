d383dbf5a94307977cd9962280ff9a86006f4174
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-24 16:16:31 +0300

    Realise CRUD operations for NoteController

--- TODO_APP.Web/Controllers/NoteController.cs
+++ TODO_APP.Web/Controllers/NoteController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TODO_APP.Service.Interfaces;
+using TODO_APP.Core.Model;
+using TODO_APP.Web.DTO;
+using System.Data;
 
 namespace TODO_APP.Web.Controllers
 {
@@ -13,5 +15,65 @@ namespace TODO_APP.Web.Controllers
             _noteService = noteService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var notes = await _noteService.GetAllAsync();
+            return View(notes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var note = await _noteService.GetByIdAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return View(note);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateNoteDto noteDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var note = new Note
+                {
+                    Title = noteDto.Title,
+                    Description = noteDto.Description,
+                    UserId = noteDto.UserId,
+                    CreatedAt = DateTime.Now
+                };
+                await _noteService.AddAsync(note);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(noteDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var note = await _noteService.GetByIdAsync(id);
+            if (note == null)
+                return NotFound();
+
+            return View(note);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var note = await _noteService.GetByIdAsync(id);
+            if (note != null) _noteService.Delete(note);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
