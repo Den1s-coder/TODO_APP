34c56d6d525f56b80d63b91409dd2e6dadad8488
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-24 15:31:19 +0300

    Update Note:
    -Rename CreateAt and UpdateAt to CreatedAt and UpdatedAt.
    -Add UserId and User for EF migrations.
    -Note constructor for EF.

--- TODO_APP.Core/Model/Note.cs
+++ TODO_APP.Core/Model/Note.cs
@@ -5,14 +5,20 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime CreateAt { get; set; } = DateTime.Now;
-        public DateTime? UpdateAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
 
-        public Note(int id, string title, string desc)
+
+        public Note() { } // for EF
+
+        public Note(int id, string title, string desc, Guid userId)
         {
             Id = id;
             Title = title;
             Description = desc;
+            UserId = userId;
         }
     }
 }
