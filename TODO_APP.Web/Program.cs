3d1730327b67dfda2dd8dcc72e1ab633b901e563
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-24 15:34:00 +0300

    Add dependencies and DBContext

--- TODO_APP.Web/Program.cs
+++ TODO_APP.Web/Program.cs
@@ -2,14 +2,23 @@ using TODO_APP.Data.Repos.Interfaces;
 using TODO_APP.Service;
 using TODO_APP.Service.Interfaces;
 using TODO_APP.Data;
+using Microsoft.EntityFrameworkCore;
+using TODO_APP.Data.Repos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<TodoDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<INoteService, NoteService>();
+
 
 var app = builder.Build();
 
