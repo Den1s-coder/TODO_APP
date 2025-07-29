c7398942a9995d1c2799a9fcfc90c591303c3e77
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-29 10:10:47 +0300

    chore: change DateTime.Now to UtcNow

--- TODO_APP.Core/Model/Note.cs
+++ TODO_APP.Core/Model/Note.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
