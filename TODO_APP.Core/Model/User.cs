a365bcdd1cc6a0af0cacbcf5f17f3aed290fa721
Denis Katasonov <diarusfoxs@gmail.com>
2025-07-31 09:53:59 +0300

    feat: add Identity User

--- TODO_APP.Core/Model/User.cs
+++ TODO_APP.Core/Model/User.cs
@@ -1,20 +1,23 @@
-﻿namespace TODO_APP.Core.Model
-{ 
-    public class User
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+﻿using Microsoft.AspNetCore.Identity;
 
-        public User(){} // for EF
-        
+namespace TODO_APP.Core.Model
+{ 
+    public class User:IdentityUser<Guid>
+    {
+        public string Name { get; set; }
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public User() { } // for EF
+
         public User(string name, string email, string password)
         {
             Id = Guid.NewGuid();
             Name = name;
             Email = email;
             PasswordHash = password;
+            Role = "User";
+            CreatedAt = DateTime.UtcNow;
         }
     }
 }
