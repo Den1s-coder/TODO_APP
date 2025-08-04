e3543602ffa6eee15d7d45c11bc74de4161c41d0
Denis Katasonov <diarusfoxs@gmail.com>
2025-08-04 10:28:22 +0300

    chore: update login/register DTOs

--- TODO_APP.Service/DTO/LoginDto.cs
+++ TODO_APP.Service/DTO/LoginDto.cs
@@ -4,5 +4,6 @@ namespace TODO_APP.Service.DTO
     {
         public string Email { get; set; }
         public string password { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
--- TODO_APP.Service/DTO/RegisterDto.cs
+++ TODO_APP.Service/DTO/RegisterDto.cs
@@ -5,5 +5,6 @@ namespace TODO_APP.Service.DTO
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
