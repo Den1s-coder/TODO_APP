c3380c929affce056a71743e83abecd2e930f8e2
Denis Katasonov <112327020+Den1s-coder@users.noreply.github.com>
2025-08-19 12:05:54 +0300

    chore: change comments

--- TODO_APP.Service/Mappings/UserProfile.cs
+++ TODO_APP.Service/Mappings/UserProfile.cs
@@ -13,14 +13,14 @@ namespace TODO_APP.Service.Mappings
     {
         public UserProfile()
         {
-            // Маппінг з User в UserDto
+            // User to UserDto
             CreateMap<User, UserDto>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
                 .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
 
-            // Маппінг з UserDto в User (якщо потрібно)
+            // UserDto to User
             CreateMap<UserDto, User>()
                 .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
